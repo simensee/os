be6ca5f474281a26c23b6c3384f27f2ae3b31fc7
simensee <simensee@gmail.com>
2022-02-09 17:32:25 +0100

    fix

--- oving1/alarmclock.c
+++ oving1/alarmclock.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <time.h>
+
 int main() {
     char c;
-    time_t current_time;
+    time_t t = time(NULL);
+    struct tm current_time = *localtime(&t);
+
+
     struct alarm {
-        time_t time;
+        struct tm time;
         int pid;
     }
     
-    printf("Welcome to the alarm clock! It is currently %d. \n", current_time);
+    printf("Welcome to the alarm clock! It is currently %d. \n", mktime(current_time));
     printf("Please enter \"c\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)");
     c = scanf("");
+
     do {
         if (c == 'l') {
             list_alarms();
         }
+
         if (c == 's') {
 
         }
-        {
-            
+
+        if (c == 'c') {
+
         }
         
         
